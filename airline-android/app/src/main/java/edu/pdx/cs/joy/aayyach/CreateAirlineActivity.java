34aeb22d3e219a86b614b008f3161e3f54e96413
Alaa Ayyach <alaa.s.ayyach@gmail.com>
2026-08-12 06:12:42 -0700

    Finished create airline screen working on create flight screen

--- airline-android/app/src/main/java/edu/pdx/cs/joy/aayyach/CreateAirlineActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs/joy/aayyach/CreateAirlineActivity.java
@@ -1,6 +1,10 @@
 package edu.pdx.cs.joy.aayyach;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pdx.cs.joy.ParserException;
+
 public class CreateAirlineActivity extends AppCompatActivity {
+
+    public static final String AIRLINE_NAME = "AIRLINE_NAME";
+    private Airline airline;
+    private final List<Airline> airlines = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,82 @@ public class CreateAirlineActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadAirlineData();
     }
+
+    public void createAirline(View view) {
+        EditText name = findViewById(R.id.airlineName);
+        String airlineName = name.getText().toString();
+
+        if (airlineName.isEmpty()) {
+            Toast.makeText(this, "Airline name cannot be empty", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        this.airline = new Airline(airlineName);
+
+        // Write airline to internal storage
+        try {
+            writeAirlinesToFile();
+            if (this.airline.getName().equals(airlineName)) {
+                Toast.makeText(this, "Airline " + airlineName + " has been created", Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            Toast.makeText(this, "While writing Airline to file: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void writeAirlinesToFile() throws IOException {
+        if (this.airline == null) { return; }
+
+        File airlinesFile = getAirlinesFile();
+        try (PrintWriter pw = new PrintWriter(new FileWriter(airlinesFile))) {
+            if (this.airline != null) {
+                TextDumper dumper = new TextDumper(pw);
+                dumper.dump(this.airline);
+            }
+            pw.flush();
+        }
+    }
+
+    private File getAirlinesFile() {
+        File dataDir = this.getDataDir();
+        return new File(dataDir, "airlines_" + this.airline.getName() + ".txt");
+    }
+
+    public void backToHome(View view) {
+        EditText airlineNameInput = findViewById(R.id.airlineName);
+        String airlineName = airlineNameInput.getText().toString();
+
+        if (this.airline != null) {
+            Intent intent = new Intent();
+            intent.putExtra(AIRLINE_NAME, airlineName);
+            setResult(RESULT_OK, intent);
+        }
+        finish();
+    }
+
+    private void loadAirlineData() {
+        String [] airlineFiles = fileList();
+        if (airlineFiles == null) { return; }
+
+        for (String file : airlineFiles) {
+            if (file.startsWith("airline_") && file.endsWith(".txt")) {
+                try (FileReader fr = new FileReader(file)) {
+                    TextParser parser = new TextParser(fr);
+                    Airline readAirline = parser.parse();
+                    if (readAirline != null) { airlines.add(readAirline); }
+                } catch (ParserException | IOException e) {
+                    Toast.makeText(this, "Could not load airline data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public void openFlightScreen(View view) {
+        Intent intent = new Intent(this, CreateFlightActivity.class);
+        startActivity(intent);
+    }
+
 }
--- airline-android/app/src/main/java/edu/pdx/cs/joy/aayyach/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs/joy/aayyach/MainActivity.java
@@ -3,14 +3,25 @@ package edu.pdx.cs.joy.aayyach;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import edu.pdx.cs.joy.ParserException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CREATE_AIRLINE_REQUEST = 1;
+    private final HashMap<String, Airline> airlines = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +33,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadAirlineData();
     }
 
     public void openAirlineScreen(View view) {
         Intent intent = new Intent(this, CreateAirlineActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, CREATE_AIRLINE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CREATE_AIRLINE_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    String airlineName = data.getStringExtra(CreateAirlineActivity.AIRLINE_NAME);
+
+                    if (airlineName != null) {
+                        loadAirlineData();
+                    }
+                }
+            }
+        }
+    }
+
+    private void loadAirlineData() {
+        String [] airlineFiles = fileList();
+        if (airlineFiles == null) { return; }
+
+        for (String file : airlineFiles) {
+            if (file.startsWith("airline_") && file.endsWith(".txt")) {
+                try (FileReader fr = new FileReader(file)) {
+//                    TextParser parser = new TextParser(new FileReader("airlines.txt"));
+                    TextParser parser = new TextParser(fr);
+
+                    Airline readAirline = parser.parse();
+                    if (readAirline != null) { airlines.put(readAirline.getName(), readAirline); }
+                } catch (ParserException | IOException e) {
+                    Toast.makeText(this, "Could not load airline data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
